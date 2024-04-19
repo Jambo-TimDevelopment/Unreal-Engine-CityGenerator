bd1254625b60adbddd9f58383622df20b0d57179
Semyon <semennikolaev9251@gmail.com>
2024-04-19 16:01:45 +0300

    Create project

---
+++ Source/CityGenerator/CityGenerator.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CityGenerator.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CityGenerator, "CityGenerator" );
---
+++ Source/CityGenerator/CityGeneratorGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CityGeneratorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CITYGENERATOR_API ACityGeneratorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
