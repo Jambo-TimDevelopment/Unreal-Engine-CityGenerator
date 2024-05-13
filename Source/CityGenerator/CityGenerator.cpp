4fd51920824c76161f6c16f396489ed4558504ca
Semyon <semennikolaev9251@gmail.com>
2024-05-13 10:32:44 +0300

    Init commit

---
+++ Source/CityGenerator/CityGenerator.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CityGenerator.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CityGenerator, "CityGenerator" );
+
+DEFINE_LOG_CATEGORY(LogCityGenerator)
+ 
---
+++ Source/CityGenerator/CityGeneratorGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CityGeneratorGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACityGeneratorGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACityGeneratorGameMode();
+};
+
+
+
